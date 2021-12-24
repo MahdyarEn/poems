5baa5bd4dbb1bd95af4af48be26b40e1f38e0e1c
Mahdyar Entezami <MahdyarEn@gmail.com>
2021-12-24 14:36:22 +0330

    🚩Change API Address

--- src/services/poems.js
+++ src/services/poems.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost/poems.php";
+const BASE_URL = "https://poem.nikan46.ir/api/poems.php";
 
 export const getPoems = async () => {
   const response = await axios.get(BASE_URL);
