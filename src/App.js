d5393951c5e8b8e012db6d41d22a0dd2a69b7126
Mahdyar Entezami <MahdyarEn@gmail.com>
2021-12-24 09:31:11 +0330

    Add poet option

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ const App = () => {
   const [data, setData] = useState([]);
   let listAllowed = useRef([]);
   let listAllowed1 = useRef([]);
-  const [btn, setBtn] = useState(true)
-
+  let listAllowed2 = useRef([]);
+  
   useEffect(() => {
     const fetchApi = async () => {
       const poems = await getPoems();
@@ -22,9 +22,10 @@ const App = () => {
     end: false,
     index: 10,
   });
-
+  
   const [selectedOption, setSelectedOption] = useState(null);
   const [selectedOption1, setSelectedOption1] = useState(null);
+  const [pt,setpt] = useState(null)
   selectedOption?.map((item) => {
     listAllowed.current = [];
     listAllowed.current.push(item.value);
@@ -36,9 +37,14 @@ const App = () => {
     listAllowed1.current = [];
     listAllowed1.current.push(item.value);
   });
+  pt?.map((item) => {
+    listAllowed2.current = [];
+    listAllowed2.current.push(item.value);
+  });
+
 
   const check = data.filter((item) => {
-    if (listAllowed.current.some((v) => item.firstLetter.includes(v)) && listAllowed1.current.some((v) => item.lastLetter.includes(v))) {
+    if (listAllowed.current.some((v) => item.firstLetter.includes(v)) && listAllowed1.current.some((v) => item.lastLetter.includes(v)) && listAllowed2.current.some((v) => item.poet.includes(v))) {
       return true;
     }
     return false;
@@ -91,38 +97,29 @@ const App = () => {
   ];
 
   const poetOptions = [
-    { value: "هوشنگ ابتهاج", label: "الف" },
-    { value: "ب", label: "ب" },
-    { value: "پ", label: "پ" },
-    { value: "ت", label: "ت" },
-    { value: "ث", label: "ث" },
-    { value: "ج", label: "ج" },
-    { value: "چ", label: "چ" },
-    { value: "ح", label: "ح" },
-    { value: "خ", label: "خ" },
-    { value: "د", label: "د" },
-    { value: "ذ", label: "ذ" },
-    { value: "ر", label: "ر" },
-    { value: "ز", label: "ز" },
-    { value: "ژ", label: "ژ" },
-    { value: "س", label: "س" },
-    { value: "ش", label: "ش" },
-    { value: "ص", label: "ص" },
-    { value: "ض", label: "ض" },
-    { value: "ط", label: "ط" },
-    { value: "ظ", label: "ظ" },
-    { value: "ع", label: "ع" },
-    { value: "غ", label: "غ" },
-    { value: "ف", label: "ف" },
-    { value: "ق", label: "ق" },
-    { value: "ک", label: "ک" },
-    { value: "گ", label: "گ" },
-    { value: "ل", label: "ل" },
-    { value: "م", label: "م" },
-    { value: "ن", label: "ن" },
-    { value: "و", label: "و" },
-    { value: "ه", label: "ه" },
-    { value: "ی", label: "ی" },
+    { value: "", label: "همه" },
+    { value: "حافظ", label: "حافظ" },
+    { value: "سعدی", label: "سعدی" },
+    { value: "مولوی", label: "مولوی" },
+    { value: "فردوسی", label: "فردوسی" },
+    { value: "نظامی", label: "نظامی" },
+    { value: "صائب", label: "صائب" },
+    { value: "عطار", label: "عطار" },
+    { value: "بیدل دهلوی", label: "بیدل دهلوی" },
+    { value: "پروین اعتصامی", label: "پروین اعتصامی" },
+    { value: "هوشنگ ابتهاج", label: "هوشنگ ابتهاج" },
+    { value: "امیرخسرو دهلوی", label: "امیرخسرو دهلوی" },
+    { value: "جامی", label: "جامی" },
+    { value: "خاقانی", label: "خاقانی" },
+    { value: "شهریار", label: "شهریار" },
+    { value: "دهلوی", label: "دهلوی" },
+    { value: "شیخ بهایی", label: "شیخ بهایی" },
+    { value: "فاضل نظری", label: "فاضل نظری" },
+    { value: "قاآنی", label: "قاآنی" },
+    { value: "فریدون مشیری", label: "فریدون مشیری" },
+    { value: "قیصر امین‌پور", label: "قیصر امین‌پور" },
+    { value: "ملک‌الشعرای بهار", label: "ملک‌الشعرای بهار" },
+    { value: "وحشی بافقی", label: "وحشی بافقی" },
   ];
 
   console.log(selectedOption);
@@ -130,7 +127,7 @@ const App = () => {
     <div>
       <Select className="basic-single" classNamePrefix="select" onChange={setSelectedOption} isMulti isDisabled={false} isLoading={false} isClearable={true} isRtl={false} isSearchable={true} name="color" options={options} />
       <Select className="basic-single" classNamePrefix="select" onChange={setSelectedOption1} isMulti isDisabled={false} isLoading={false} isClearable={true} isRtl={false} isSearchable={true} name="color2" options={options} />
-      <Select className="basic-single" classNamePrefix="select" onChange={setSelectedOption1} isMulti isDisabled={false} isLoading={false} isClearable={true} isRtl={false} isSearchable={true} name="color2" options={options} />
+      <Select className="basic-single" classNamePrefix="select" onChange={setpt} isMulti isDisabled={false} isLoading={false} isClearable={true} isRtl={false} isSearchable={true} name="color2" options={poetOptions} />
       {dch()?.map((data) => (
         <p>{data.hemistich1}</p>
       ))}
